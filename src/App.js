b5f994504c247548c5b887814826062c0ba83671
unknown <lizzie.olishuk1@gmail.com>
2021-10-28 12:59:48 +0300

    delete file function

--- src/App.js
+++ src/App.js
@@ -20,33 +20,6 @@ function App() {
   const [Subject, setSubject] = useState("");
   const [Message, setMessage] = useState("home");
 
-  function downloadCvFile() {
-    var oReq = new XMLHttpRequest();
-    // The Endpoint of your server
-    var URLToPDF = "../public/lizziesCv.pdf";
-
-    // Configure XMLHttpRequest
-    oReq.open("GET", URLToPDF, true);
-
-    // Important to use the blob response type
-    oReq.responseType = "blob";
-
-    // When the file request finishes
-    // Is up to you, the configuration for error events etc.
-    oReq.onload = function () {
-      // Once the file is downloaded, open a new window with the PDF
-      // Remember to allow the POP-UPS in your browser
-      var file = new Blob([oReq.response], {
-        type: "application/pdf",
-      });
-
-      // Generate file download directly in the browser !
-      saveAs(file, "lizziesCv.pdf");
-    };
-
-    oReq.send();
-  }
-
   const sendInfo = (e) => {
     e.preventDefault();
 
